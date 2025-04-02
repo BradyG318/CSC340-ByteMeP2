e24da3969f6a09b40e40afec0bbb90adb9309672
BradyG318 <160978995+BradyG318@users.noreply.github.com>
2025-04-02 12:11:31 -0400

    ForgotToSaveLiterallyEverything

--- Gamelogic/GameManager.java
+++ Gamelogic/GameManager.java
@@ -9,5 +9,7 @@ package Gamelogic;
  *  and the steadily reducing point total for how long it takes players to answer)
  */
 public class GameManager {
-    
+    public static void startRound() {
+        
+    }
 }
---
+++ Gamelogic/Question.java
@@ -0,0 +1,19 @@
+package Gamelogic;
+/**
+ * Question object containing multiple Strings and string arrays including
+ */
+public class Question {
+    String question;
+    String[] answers;
+    int correctAns;
+    /**
+     * @param question {@code String} object of the question being asked
+     * @param answers {@code String[]} list of potential answers to the question
+     * @param correctAns {@code int} value representing the index of the correct answer 
+     */
+    public Question(String question, String[] answers, int correctAns) {
+        this.question = question;
+        this.answers = answers;
+        this.correctAns = correctAns;
+    }
+}

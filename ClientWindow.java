d07db0225bfa8f05f1374fdacb5ed7a940351f5d
asHasHasHas <168456398+asHasHasHas@users.noreply.github.com>
2025-04-06 11:42:43 -0400

    client window update

--- ClientWindow.java
+++ ClientWindow.java
@@ -2,54 +2,65 @@
 import java.awt.Color;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
 import java.security.SecureRandom;
-import java.util.TimerTask;
 import java.util.Timer;
+import java.util.TimerTask;
 import javax.swing.*;
 
 public class ClientWindow implements ActionListener
 {
 	private JButton poll;
-	private JButton submit;
-	private JRadioButton options[];
-	private ButtonGroup optionGroup;
-	private JLabel question;
-	private JLabel timer;
-	private JLabel score;
-	private TimerTask clock;
-	private int scoreNum;
-	
-	private JFrame window;
+    private JButton submit;
+    private JRadioButton options[];
+    private ButtonGroup optionGroup;
+    private JLabel question;
+    private JLabel timer;
+    private JLabel score;
+    private TimerTask clock;
+    private int scoreNum;
+
+    private JFrame window;
+    private DatagramSocket socket;
+    private InetAddress serverAddress;
+    private int serverPort = 5000;
+    private boolean pollAllowed = true;
+    private boolean hasPolled = false;
+    private int selectedAnswer = -1;
 	
 	private static SecureRandom random = new SecureRandom();
 	
 	// write setters and getters as you need
 	
-	public ClientWindow()
+	public ClientWindow(String serverIp)
 	{
+		try {
+            socket = new DatagramSocket();
+            serverAddress = InetAddress.getByName(serverIp);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
 		JOptionPane.showMessageDialog(window, "Video Game Trivia");
-		
 		window = new JFrame("Video game Trivia");
 
-		//read question from protocol tcp
-		question = new JLabel("Q1. This is a sample question"); // represents the question
-		window.add(question);
-		question.setBounds(10, 5, 350, 100);;
+
+
+		question = new JLabel("Waiting for question...");
+        question.setBounds(10, 5, 350, 100);
+        window.add(question);
 		
 		options = new JRadioButton[4];
-		optionGroup = new ButtonGroup();
-
-		//for each index, read option from protocol tcp
-		//parse with '.'
-		for(int index=0; index<options.length; index++)
-		{
-			options[index] = new JRadioButton("Option " + (index+1));  // represents an option
-			// if a radio button is clicked, the event would be thrown to this class to handle
-			options[index].addActionListener(this);
-			options[index].setBounds(10, 110+(index*20), 350, 20);
-			window.add(options[index]);
-			optionGroup.add(options[index]);
-		}
+        optionGroup = new ButtonGroup();
+        for (int index = 0; index < options.length; index++) {
+            options[index] = new JRadioButton("Option " + (index + 1));
+            options[index].addActionListener(this);
+            options[index].setBounds(10, 110 + (index * 20), 350, 20);
+            window.add(options[index]);
+            optionGroup.add(options[index]);
+        }
 
 		timer = new JLabel("TIMER");  // represents the countdown shown on the window
 		timer.setBounds(250, 250, 100, 20);
@@ -91,75 +102,80 @@ public class ClientWindow implements ActionListener
 		
 		// input refers to the radio button you selected or button you clicked
 		String input = e.getActionCommand();  
-		switch(input)
-		{
-			case "Option 1":	// Your code here   store/change answer in udp packet
-								break;
-			case "Option 2":	// Your code here	
-								break;
-			case "Option 3":	// Your code here
-								break;
-			case "Option 4":	// Your code here
-								break;
-			case "Poll":		// Your code here send   udp to server, freeze all options
-								break;
-			case "Submit":		// Your code here	if poll = 1 able to submit (at end of timer?)
-								break;
-			default:
-								System.out.println("Incorrect Option");
-		}
-		
-		// test code below to demo enable/disable components
-		// DELETE THE CODE BELOW FROM HERE***
-		if(poll.isEnabled())
-		{
-			poll.setEnabled(false);
-			submit.setEnabled(true);
-		}
-		else
-		{
-			poll.setEnabled(true);
-			submit.setEnabled(false);
-		}
-		
-		question.setText("Q2. This is another test problem " + random.nextInt());
-		
-		// you can also enable disable radio buttons
-//		options[random.nextInt(4)].setEnabled(false);
-//		options[random.nextInt(4)].setEnabled(true);
-		// TILL HERE ***
-		
-	}
+		switch (input) {
+            case "Option 1":
+                selectedAnswer = 0;
+                break;
+            case "Option 2":
+                selectedAnswer = 1;
+                break;
+            case "Option 3":
+                selectedAnswer = 2;
+                break;
+            case "Option 4":
+                selectedAnswer = 3;
+                break;
+            case "Poll":
+                if (pollAllowed) {
+                    sendUDP("POLL"); //place holder name 
+                    hasPolled = true;
+                    disableAll();
+                    submit.setEnabled(true);
+                    poll.setEnabled(false);
+                } else {
+                    sendUDP("TOO_LATE"); //place holder name 
+                }
+                break;
+            case "Submit":
+                if (hasPolled && selectedAnswer != -1) {
+                    sendUDP("ANSWER:" + selectedAnswer); //place holder name 
+                    disableAll();
+                }
+                break;
+            default:
+                System.out.println("Incorrect Option");
+        }
+    }
+	private void sendUDP(String msg) {
+        try {
+            byte[] buffer = msg.getBytes();
+            DatagramPacket packet = new DatagramPacket(buffer, buffer.length, serverAddress, serverPort);
+            socket.send(packet);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void disableAll() {
+        for (JRadioButton option : options) {
+            option.setEnabled(false);
+        }
+    }
 	
 	// this class is responsible for running the timer on the window
-	public class TimerCode extends TimerTask
-	{
-		private int duration;  // write setters and getters as you need
-		public TimerCode(int duration)
-		{
-			this.duration = duration;
-		}
-		@Override
-		public void run()
-		{
-			if(duration < 0)
-			{
-				timer.setText("Timer expired");
-				window.repaint();
-				this.cancel();  // cancel the timed task
-				return;
-				// you can enable/disable your buttons for poll/submit here as needed
-			}
-			
-			if(duration < 6)
-				timer.setForeground(Color.red);
-			else
-				timer.setForeground(Color.black);
-			
-			timer.setText(duration+"");
-			duration--;
-			window.repaint();
-		}
-	}
+	public class TimerCode extends TimerTask {
+        private int duration;
+
+        public TimerCode(int duration) {
+            this.duration = duration;
+        }
+
+        @Override
+        public void run() {
+            if (duration < 0) {
+                timer.setText("Timer expired");
+                window.repaint();
+                this.cancel();
+                pollAllowed = false;
+                submit.setEnabled(false);
+                return;
+            }
+
+            timer.setForeground(duration < 6 ? Color.red : Color.black);
+            timer.setText(duration + "");
+            duration--;
+            window.repaint();
+        }
+    }
 	
 }
